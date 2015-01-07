a8194f7b5a2d54b870103ccc97ee095daaba7be9
joskarthic <joskarthick3@gmail.com>
2015-01-07 14:51:01 +0530

    Update CameraManager.java

--- src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
+++ src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
@@ -39,8 +39,8 @@ public final class CameraManager {
 
   private static final String TAG = CameraManager.class.getSimpleName();
 
-  private static final int MIN_FRAME_WIDTH = 1240;
-  private static final int MIN_FRAME_HEIGHT = 1400;
+  private static final int MIN_FRAME_WIDTH = 540;
+  private static final int MIN_FRAME_HEIGHT = 500;
   private static final int MAX_FRAME_WIDTH = 1600;
   private static final int MAX_FRAME_HEIGHT = 1400;
 
